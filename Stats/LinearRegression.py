f8c8aac47db96abb159fee55e154d9755d314d0d
Colin Stadig <colinstadig@hotmail.com>
2022-09-01 12:43:46 -0600

    Initial push

--- Stats/LinearRegression.py
+++ Stats/LinearRegression.py
@@ -1,9 +1,14 @@
-from re import X
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
 x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
 y = np.array([5, 20, 14, 32, 22, 38])
+
+model = LinearRegression()
 
 print(x)
 print(y)
